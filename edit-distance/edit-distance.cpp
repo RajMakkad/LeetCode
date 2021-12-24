0e2dce74a4f0cdcd6436f10e3d8731b3645c878c
RajMakkad <74362791+RajMakkad@users.noreply.github.com>
2021-12-24 16:08:41 +0530

    Time: 12 ms (66.45%), Space: 9.3 MB (14.23%) - LeetHub

---
+++ edit-distance/edit-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int memoization(int n,int m,string &s1,string &s2,vector<vector<int>> &dp){
+        if(n==0) return dp[n][m] = m;
+        else if(m==0) return dp[n][m] = n;
+
+        if(dp[n][m]!=-1) return dp[n][m];
+        int x = INT_MAX;
+        int y = INT_MAX;
+        int z = INT_MAX;
+
+        if(s1[n - 1]==s2[m - 1]) return dp[n][m] = memoization(n - 1,m - 1,s1,s2,dp); // same.
+        else{
+            x = memoization(n,m - 1,s1,s2,dp) + 1; // insert.
+            y = memoization(n - 1,m,s1,s2,dp) + 1; // delete.
+            z = memoization(n - 1,m - 1,s1,s2,dp) + 1; // replace.
+        }
+
+        return dp[n][m] = min({x,y,z});
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        vector<vector<int>> dp(n + 1,vector<int>(m + 1,-1));
+        memoization(n,m,word1,word2,dp);
+        return dp[n][m];
+    }
+};
