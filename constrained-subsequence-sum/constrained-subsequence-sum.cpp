5141b26167908f9d3058c53bcb63b6163dbb4e28
RajMakkad <74362791+RajMakkad@users.noreply.github.com>
2021-12-26 15:28:13 +0530

    Time: 312 ms (42.58%), Space: 139.4 MB (29.30%) - LeetHub

--- constrained-subsequence-sum/constrained-subsequence-sum.cpp
+++ constrained-subsequence-sum/constrained-subsequence-sum.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
         int n = nums.size();
-        deque<int> q;
+        priority_queue<pair<int,int>> q;
         
-        int ans = nums[0];
+        int ans = -1e4;
         
         for(int i = 0;i<n;i++){
-            if(q.size()) nums[i] += nums[q.front()];
+            while(q.size() and i - q.top().second > k) q.pop();
+            if(q.size()) nums[i] += q.top().first;
+            
             ans = max(ans,nums[i]);
-            
-            while(q.size() and nums[i] > nums[q.back()]) q.pop_back();
-            if(nums[i]>0) q.push_back(i);
-            
-            if(q.size() and q.front() == i - k) q.pop_front();
+            if(nums[i]>0) q.push({nums[i],i});
         }
         
         return ans;
