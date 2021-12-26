ea8cced5539a0514de6178683552b91fd23f9db8
RajMakkad <74362791+RajMakkad@users.noreply.github.com>
2021-12-26 15:31:39 +0530

    Update constrained-subsequence-sum.cpp
    Two different solutions
    1. Using Deque .
    2. Using Priority queue.

--- constrained-subsequence-sum/constrained-subsequence-sum.cpp
+++ constrained-subsequence-sum/constrained-subsequence-sum.cpp
@@ -1,3 +1,30 @@
+// Deque
+class Solution {
+public:
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> q;
+        
+        int ans = nums[0];
+        
+        for(int i = 0;i<n;i++){
+            if(q.size()) nums[i] += nums[q.front()];
+            ans = max(ans,nums[i]);
+            
+            while(q.size() and nums[i] > nums[q.back()]) q.pop_back();
+            if(nums[i]>0) q.push_back(i);
+            
+            if(q.size() and q.front() == i - k) q.pop_front();
+        }
+        
+        return ans;
+    }
+};
+// o(n)
+
+
+
+// Max priority queue.
 class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
@@ -17,3 +44,4 @@ public:
         return ans;
     }
 };
+// o(nlogn)
