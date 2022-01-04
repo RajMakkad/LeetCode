e0aacfd655f66e34a129e716b1dee91c509747f1
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-05 02:27:24 +0530

    Time: 11 ms (16.20%), Space: 7.2 MB (40.27%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,30 +1,10 @@
 class Solution {
-	bool is_valid(string &str1, string &str2, string &ans){
-		string s = "";
-		while(s.length() < str1.length()) s += ans;
-		if(s != str1) return false;
-
-		s = "";
-		while(s.length() < str2.length()) s += ans;
-		if(s != str2) return false;
-
-		return true;
-	}
 public:
     string gcdOfStrings(string str1, string str2) {
-		int n = str1.length();
-		int m = str2.length();
-		int k = min(n,m);
-        string ans = "";
-
-		for(int i = 0;i<k;i++){
-			if(str1[i] == str2[i]){
-				string s = str1.substr(0,i + 1);
-				if(is_valid(str1,str2,s)) ans = s;
-			}
-			else return "";
-		}
-
-		return ans;
+        int n = str1.length();
+        int m = str2.length();
+        
+        if(str1 + str2 == str2 + str1) return str1.substr(0,__gcd(n,m));
+        else return "";
     }
 };
