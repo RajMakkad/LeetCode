36dc5debefb2ddaafc96de03f7e2de17370c4cb6
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-06 16:06:25 +0530

    Time: 27 ms (18.02%), Space: 10.4 MB (29.36%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,18 +1,34 @@
 class Solution {    
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int> rangeQuery(1001,0);
+//         vector<int> rangeQuery(1001,0);
 
+//         for(auto i:trips){
+//             rangeQuery[i[1]] += i[0];
+//             rangeQuery[i[2]] -= i[0];
+//         }
+
+//         if(rangeQuery[0] > capacity) return false;
+
+//         for(int i = 1;i<=1000;i++){
+//             rangeQuery[i] += rangeQuery[i - 1];
+//             if(rangeQuery[i] > capacity) return false;
+//         }
+
+//         return true;
+        
+        vector<pair<int,int>> track;
         for(auto i:trips){
-            rangeQuery[i[1]] += i[0];
-            rangeQuery[i[2]] -= i[0];
+            track.push_back({i[1],i[0]});
+            track.push_back({i[2],-i[0]});
         }
 
-        if(rangeQuery[0] > capacity) return false;
+        sort(track.begin(),track.end());
+        int passengers = 0;
 
-        for(int i = 1;i<=1000;i++){
-            rangeQuery[i] += rangeQuery[i - 1];
-            if(rangeQuery[i] > capacity) return false;
+        for(auto i:track){
+            passengers += i.second;
+            if(passengers > capacity) return false;
         }
 
         return true;
