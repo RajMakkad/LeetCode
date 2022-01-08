b280af307798f02aa28f147e24669864d4b54881
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-08 15:12:47 +0530

    Time: 97 ms (56.19%), Space: 31.5 MB (31.48%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -8,18 +8,18 @@ public:
 
         for(int i = 0;i<n;i++) mp[nums[i]] = i;
 
-        for(auto &i:mp){
-            if(vis[i.second]) continue;
+        for(auto &i:nums){
+            if(vis[mp[i]]) continue;
             int count = 1;
 
-            int num = i.first + 1;
+            int num = i + 1;
             while(mp.find(num) != mp.end()){
                 vis[mp[num]] = 1;
                 num++;
                 count++;
             }
 
-            num = i.first - 1;
+            num = i - 1;
             while(mp.find(num) != mp.end()){
                 vis[mp[num]] = 1;
                 num--;
