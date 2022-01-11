7883f032613677f723df1f86dcadb144af140d21
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-11 16:39:52 +0530

    Time: 8 ms (53.68%), Space: 16.7 MB (72.35%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,23 +1,9 @@
+
 class Solution {
-    void getsum(TreeNode *root,int &num,int &ans){
-        if(!root) return ;
-        if(!root->left and !root->right){
-            ans += (num<<1) + root->val;
-            return ;
-        }
-        
-        num = num<<1;
-        num += root->val;
-        getsum(root->left,num,ans);
-        getsum(root->right,num,ans);
-        num /= 2;
-    }
 public:
-    int sumRootToLeaf(TreeNode* root) {
-        int ans = 0;
-        int num = 0;
-        
-        getsum(root,num,ans);
-        return ans;
+    int sumRootToLeaf(TreeNode* root,int num = 0) {
+        if(!root) return 0;
+        num = num*2 + root->val;
+        return root->left == root->right ? num : sumRootToLeaf(root->left,num) + sumRootToLeaf(root->right,num);
     }
 };
