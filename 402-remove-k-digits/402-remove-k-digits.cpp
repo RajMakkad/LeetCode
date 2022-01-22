a0fc521a6abae7724e23e3a104ccbbe4d02bd86f
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-23 02:37:49 +0530

    Time: 11 ms (33.33%), Space: 6.9 MB (85.37%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     string removeKdigits(string num, int k){
-        int n = num.length();
-        vector<vector<int>> dp(n,vector<int>(10,-1));
-
-        dp[n - 1][num[n - 1] - '0'] = n - 1;
-        for(int i = n - 2;i>=0;i--){
-            dp[i] = dp[i + 1];
-            dp[i][num[i] - '0'] = i;
-        }
-
         string s = "";
-        for(int i = 0;i<n;i++){
-            int e = num[i] - '0';
-            for(int d = 0;d<e;d++){
-                if(dp[i][d] != -1 and k >= dp[i][d] - i){
-                    k = k - (dp[i][d] - i);
-                    i = dp[i][d];
-                    break;
-                }
+        for(auto &i:num){
+            while(k and !s.empty() and s.back() > i){
+                s.pop_back();
+                k--;
             }
-
-            if(s.empty() and num[i] == '0') continue;
-            s += num[i];
+            if(s.empty() and i == '0') continue;
+            s += i;
         }
 
         while(k and s.length()){
@@ -30,7 +16,7 @@ public:
             s.pop_back();
         }
         if(s.empty()) s += '0';
+
         return s; 
     }
 };
-
