bcca2943ccc0e05a6be361c993de803009223be5
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-26 15:45:51 +0530

    Time: 312 ms (6.64%), Space: 84.4 MB (91.67%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -9,22 +9,12 @@ class Solution {
     }
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2){
-        vector<int> v1,v2,ans;
-        getElements(root1,v1);
-        getElements(root2,v2);
+        vector<int> v;
+        getElements(root1,v);
+        getElements(root2,v);
         
-        int a = 0;
-        int b = 0;
+        sort(v.begin(),v.end());
         
-        while(a < v1.size() || b < v2.size()){
-            if(a < v1.size() and b < v2.size()){
-                if(v1[a] > v2[b]) ans.push_back(v2[b++]);
-                else ans.push_back(v1[a++]);
-            }
-            else if(a < v1.size()) ans.push_back(v1[a++]);
-            else ans.push_back(v2[b++]);
-        }
-        
-        return ans;
+        return v;
     }
 };
