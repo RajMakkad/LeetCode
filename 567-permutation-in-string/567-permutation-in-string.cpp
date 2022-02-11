1b0189661cd9b4b1ed46318779cb57ca26c4c74a
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-02-11 15:49:05 +0530

    Time: 6 ms (83.05%), Space: 7.2 MB (71.57%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if(n>m) return false;
+        vector<int> hash(26,0);
+        vector<int> temp(26,0);
+
+        for(auto i:s1) hash[(i - 'a')]++;
+        for(int i = 0;i<m;i++){
+            if(i < n - 1) temp[(s2[i] - 'a')]++;
+            else{
+                temp[(s2[i] - 'a')]++;
+                if(temp == hash) return true;
+                temp[(s2[i - n + 1] - 'a')]--;
+            }
+        }
+
+        return false;
+    }
+};
