45490373242caaf01395c5613dfb85fe7d0c2279
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-03-23 03:00:06 +0530

    Time: 204 ms (13.83%), Space: 71.6 MB (8.03%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    unordered_map<int,int> mp;
-    int dp[6001][2];
-    
-    
-    int helper(int i, bool back , int a ,int b, int x) {
-        if(i == x)
-            return 0;
-        if(i<0 || i>6000 || back>=2 || mp.find(i)!=mp.end())
-            return 1e9;
-        if(dp[i][back] != -1) 
-            return dp[i][back];
-        
-        dp[i][back] = 1+helper(i+a,0,a,b,x); //go forward
-        
-        if(!back) //cannot go consecutively backwards more than 1
-        {
-        dp[i][back] = min(dp[i][back] , helper(i-b,1,a,b,x)+1);  
-        
-        }
-        return dp[i][back];
-    }
-    
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x){
+        int n = 6000;
+        vector<vector<int>> vis(n + 1,vector<int>(2,1e9));
+        unordered_map<int,int> end;
+        for(auto &i:forbidden) end[i] = 1;
 
-    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-        for(int i=0;i<forbidden.size();i++){
-            mp[forbidden[i]] = 1;
+        queue<pair<int,int>> q;
+        if(end[0] != 1){
+            vis[0][0] = vis[0][1] = 0;
+            q.push({0,0});
         }
-        memset(dp,-1,sizeof(dp));
-        int ans = helper(0,0,a,b,x);
-        if(ans>1e9) 
-               return -1;
-        return ans;               
+
+        while(!q.empty()){
+            int pos = q.front().first;
+            int u = q.front().second;
+            q.pop();
+
+            int forward = pos + a;
+            int backward = pos - b;
+            if(forward <= n and vis[forward][0] > vis[pos][u] + 1 and end[forward] != 1){
+                vis[forward][0] = vis[pos][u] + 1;
+                q.push({forward,0});
+            }
+
+            if(u == 1) continue;
+            if(backward >= 0 and vis[backward][1] > vis[pos][u] + 1 and end[backward] != 1){
+                vis[backward][1] = vis[pos][u] + 1;
+                q.push({backward,1});
+            }
+        }
+
+        int ans = min(vis[x][0],vis[x][1]);
+        return ans != 1e9 ? ans : -1;
     }
 };
