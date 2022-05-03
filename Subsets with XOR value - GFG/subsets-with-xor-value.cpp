b67e60c3e371ca8de01c7087fc4339fe82196aec
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-05-03 20:45:50 +0530

    Added solution - LeetHub

--- Subsets with XOR value - GFG/subsets-with-xor-value.cpp
+++ Subsets with XOR value - GFG/subsets-with-xor-value.cpp
@@ -5,22 +5,39 @@ using namespace std;
 
  // } Driver Code Ends
 
+// class Solution{
+//     int memoization(int n,int k,vector<int> &a,vector<vector<int>> &dp){
+//         if(n == 0){
+//             return k == 0;
+//         }
+//         if(dp[n][k] != -1) return dp[n][k];
+
+//         dp[n][k] = memoization(n - 1,(k ^ a[n - 1]),a,dp);
+//         dp[n][k] += memoization(n - 1,k,a,dp);
+
+//         return dp[n][k];
+//     }
+// public:
+//     int subsetXOR(vector<int> a, int n, int k) {
+//         vector<vector<int>> dp(n + 1,vector<int>(128,-1));
+//         return memoization(n,k,a,dp);
+//     }
+// };
+
 class Solution{
-    int memoization(int n,int k,vector<int> &a,vector<vector<int>> &dp){
-        if(n == 0){
-            return k == 0;
-        }
-        if(dp[n][k] != -1) return dp[n][k];
-
-        dp[n][k] = memoization(n - 1,(k ^ a[n - 1]),a,dp);
-        dp[n][k] += memoization(n - 1,k,a,dp);
-
-        return dp[n][k];
-    }
 public:
     int subsetXOR(vector<int> a, int n, int k) {
-        vector<vector<int>> dp(n + 1,vector<int>(128,-1));
-        return memoization(n,k,a,dp);
+        vector<vector<int>> dp(n + 1,vector<int>(128,0));
+
+        dp[0][0] = 1;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<128;j++){
+                dp[i + 1][j] += dp[i][j];
+                dp[i + 1][(a[i] ^ j)] += dp[i][j];
+            }
+        }
+
+        return dp[n][k];
     }
 };
 
