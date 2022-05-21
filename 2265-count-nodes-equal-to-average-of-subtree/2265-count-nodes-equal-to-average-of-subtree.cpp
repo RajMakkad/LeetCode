7c3f3bf3283ebf865dfba7b28f2b24b8b44b9951
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-05-21 21:17:36 +0530

    Time: 15 ms (45.89%), Space: 22.3 MB (10.04%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int ans = 0;
+    vector<int> avgNode(TreeNode *node){
+        if(!node) return {0,0};
+        
+        if(!node->left and !node->right){
+            ans++;
+            return {node->val,1};
+        }
+        
+        auto l = avgNode(node->left);
+        auto r = avgNode(node->right);
+        
+        int sum = l[0] + node->val + r[0];
+        int count = l[1] + 1 + r[1];
+        
+        if(sum/count == node->val) ans++;
+        return {sum,count};
+    }
+public:
+    int averageOfSubtree(TreeNode* root) {
+        avgNode(root);
+        return ans;
+    }
+};
