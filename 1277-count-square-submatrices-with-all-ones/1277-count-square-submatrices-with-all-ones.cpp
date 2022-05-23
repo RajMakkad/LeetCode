c2a2561b567d343335112be776e1e195ba266920
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-05-23 21:12:17 +0530

    Time: 153 ms (13.02%), Space: 26.4 MB (7.80%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -12,19 +12,8 @@ public:
             for(int c = 1;c<=m;c++){
 
                 if(mat[r - 1][c - 1] == 1){
-                    int p1 = 0, p2 = 0;
 
-                    for(int k = c - 1;k>0;k--){
-                        if(mat[r - 1][k - 1] == 1) p1++;
-                        else break;
-                    }
-
-                    for(int k = r - 1;k>0;k--){
-                        if(mat[k - 1][c - 1] == 1) p2++;
-                        else break;
-                    }
-
-                    dp[r][c] = min({dp[r - 1][c - 1], p1, p2}) + 1;
+                    dp[r][c] = min({dp[r - 1][c - 1], dp[r - 1][c], dp[r][c - 1]}) + 1;
                     ans += dp[r][c];
                 }
             }
