3146c8df5a09950e25b2ee36e9880ebfa8443a96
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-06-06 01:48:54 +0530

    Added solution - LeetHub

---
+++ Count pairs in array divisible by K - GFG/count-pairs-in-array-divisible-by-k.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    long long countKdivPairs(int a[], int n, int k){
+        map<int,int> mp;
+        long long ans = 0;
+
+        for(int i = 0;i<n;i++){
+            a[i] %= k;
+            int x = (k - a[i])%k;
+            
+            ans += mp[x];
+            mp[a[i]]++;
+        }
+
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+
+		int a[n];
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+
+		int k;
+		cin >> k;
+
+        Solution ob;
+		cout << ob. countKdivPairs(a, n , k) << "\n";
+
+
+
+	}
+
+
+	return 0;
+}
+  // } Driver Code Ends
