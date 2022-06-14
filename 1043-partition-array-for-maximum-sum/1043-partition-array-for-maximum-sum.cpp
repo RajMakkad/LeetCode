bddf0b03f2d7e01d11bf5105da1b50e0b046082a
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-06-14 18:26:00 +0530

    Time: 27 ms (42.94%), Space: 8.7 MB (33.58%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,19 @@
 class Solution {
-    int memoization(int x, int k, vector<int> &dp, vector<int> &a){
-        if(x == 0) return 0;
-        if(dp[x] != -1) return dp[x];
-
-        int sum = 0;
-        int max_ = 0;
-
-        for(int i = x;i >= max(x - k + 1, 1); i--){
-            max_ = max(max_, a[i - 1]);
-            sum = max_ * (x - i + 1) + memoization(i - 1, k, dp, a);
-
-            dp[x] = max(dp[x], sum);
-        }
-
-        return dp[x];
-    }
 public:
     int maxSumAfterPartitioning(vector<int>& a, int k) {
         int n = a.size();
-        vector<int> dp(n + 1, -1);
-        return memoization(n, k, dp, a);
+        vector<int> dp(n + 1, 0);
+
+        for(int i = 1;i <= n;i++){
+            int max_ = 0;
+            for(int j = i;j >= max(i - k + 1, 1); j--){
+                max_ = max(max_, a[j - 1]);
+                int sum = max_ * (i - j + 1);
+
+                dp[i] = max(dp[i], sum + dp[j - 1]);
+            }
+        }
+
+        return dp[n];
     }
 };
