72074ef199caffbef28c55376754bcfc89286de7
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-06-15 13:46:15 +0530

    Time: 186 ms (32.90%), Space: 13.1 MB (94.23%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -14,31 +14,28 @@ class Solution {
 
         return x == a.length();
     }
-    int memoization(int x, vector<int> &dp, vector<string> &w){
-        if(dp[x] != -1) return dp[x];
-
-        int ans = 0;
-        for(int i = x + 1; i < w.size(); i++){
-            if(w[i].size() > w[x].size() + 1) break;
-
-            if(w[i].size() == w[x].size() + 1 and isChain(w[x], w[i])){
-                ans = max(ans, memoization(i, dp, w));
-            }
-        }
-
-        return dp[x] = ans + 1;
-    }
 public:
     int longestStrChain(vector<string>& w) {
         int n = w.size();
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n, 0);
         sort(w.begin(), w.end(), comp);
 
         int ans = 0;
-        for(int i = 0; i<n; i++){
-            ans = max(ans, memoization(i, dp, w));
-        }
 
+        for(int i = n - 1; i >= 0; i--){
+            int sum = 0;
+            for(int j = i + 1;j < n;j++){
+                if(w[i].length() + 1 < w[j].length()) break;
+                
+                if(w[i].length() + 1 == w[j].length() and isChain(w[i], w[j])){
+                    sum = max(sum, dp[j]);
+                }
+            }
+
+            dp[i] = sum + 1;
+            ans = max(ans, dp[i]);
+        }
+        
         return ans;
     }
 };
