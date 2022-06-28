6a35ecad716a3adfd012cc9905d50f625224bed9
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-06-28 19:11:36 +0530

    Time: 90 ms (53.65%), Space: 17.3 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     int minDeletions(string s) {
         int n = s.length();
-        vector<int> mp(n + 1, 0);
-        sort(s.begin(), s.end());
+        vector<int> freq(26, 0);
         
-        int cnt = 1;
-        
-        for(int i = 1;i< n;i++){
-            if(s[i] == s[i - 1]) cnt++;
-            else {
-                mp[cnt]++;
-                cnt = 1;
-            }
+        for(int i = 0;i < n;i++){
+            int x = s[i] - 'a';
+            freq[x]++;
         }
         
-        mp[cnt]++;
+        sort(freq.begin(), freq.end());
         
         int ans = 0;
         
-        for(int i = n;i > 0;i--){
-            int x = mp[i];
-            if(x > 1){
-                ans += mp[i] - 1;
-                mp[i - 1] += mp[i] - 1;
+        for(int i = 24;i >= 0;i--){
+            if(freq[i] == 0) break;
+            
+            if(freq[i] >= freq[i + 1]){
+                int p = max(freq[i + 1] - 1, 0);
+                ans += freq[i] - p;
+                freq[i] = p;
             }
         }
         
