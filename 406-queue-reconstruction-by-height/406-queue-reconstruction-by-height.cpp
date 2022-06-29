22ed6cfc5c04128edd12e811c2cd5a0dc3bba160
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-06-30 00:16:57 +0530

    Time: 176 ms (58.63%), Space: 11.6 MB (99.33%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,26 +1,28 @@
 class Solution {
-static bool comp(const vector<int> &a,const vector<int> &b){
-	if(a[0] == b[0]) return a[1] < b[1];
-	return a[0] < b[0];
-}
+    static bool comp(const vector<int> &a,const vector<int> &b){
+        if(a[0] == b[0]) return a[1] < b[1];
+        return a[0] < b[0];
+    }
 public:
-    vector<vector<int>> reconstructQueue(vector<vector<int>>& people){
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         int n = people.size();
-        vector<vector<int>> q(n);
-        sort(people.begin(),people.end(),comp);
-        
-        for(int i = 0;i<n;i++){
-            int pos = people[i][1];
-            for(int p = 0;p<n;p++){
-                if(pos == 0 and q[p].empty()){
-                    q[p] = people[i];
+        sort(people.begin(), people.end(), comp);
+
+        vector<vector<int>> ans(n);
+
+        for(int i = 0;i < n;i++){
+            int idx = people[i][1];
+
+            for(int k = 0;k < n;k++){
+                if(idx == 0 and ans[k].empty()){
+                    ans[k] = people[i];
                     break;
                 }
-                
-                if(q[p].empty() || q[p][0] >= people[i][0]) pos--;
+
+                if(ans[k].empty() || ans[k][0] >= people[i][0]) idx--;
             }
         }
-        
-        return q;
+
+        return ans;
     }
 };
