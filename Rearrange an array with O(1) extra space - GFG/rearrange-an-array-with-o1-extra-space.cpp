7cb2d99a0e4f6dc1973f2eec0785ff5736459993
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-07-15 16:20:08 +0530

    Added solution - LeetHub

---
+++ Rearrange an array with O(1) extra space - GFG/rearrange-an-array-with-o1-extra-space.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+    
+
+class Solution{
+    public:
+    void arrange(long long arr[], int n) {
+        for(int i = 0;i < n;i++){
+            long long x = arr[arr[i]] % n;
+            arr[i] += n * x;
+        }
+        
+        for(int i = 0;i < n;i++){
+            arr[i] /= n;
+        }
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin>>t;
+    while(t--){
+        
+        int n;
+        //size of array
+        cin>>n;
+        long long A[n];
+        
+        //adding elements to the array
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+        Solution ob;
+        //calling arrange() function
+        ob.arrange(A, n);
+        
+        //printing the elements 
+        for(int i=0;i<n;i++){
+            cout << A[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
