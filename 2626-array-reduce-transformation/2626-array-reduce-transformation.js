3839b38e26d180b7dbf83960ce8b6e473c8620a6
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2024-05-05 19:12:41 +0530

    Time: 50 ms (76.78%), Space: 49.3 MB (44.08%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -1,7 +1,3 @@
 var reduce = function(nums, fn, init) {
-    let ans = init;
-    for(let i = 0;i < nums.length;i++){
-        ans = fn(ans, nums[i]);
-    }
-    return ans;
+  return nums.reduce((acc, element) => fn(acc, element), init);
 };
