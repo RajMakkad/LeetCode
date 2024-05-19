0bfed5d7ba1332dbae9a9bb2c82f0ddbb5298788
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2024-05-19 18:08:58 +0530

    Time: 202 ms (40.44%), Space: 125.6 MB (88.89%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -1,39 +1,12 @@
-#define ll long long
-
 class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        ll n = nums.size();
-        vector<vector<ll>> arr(n, vector<ll>(3));
-        int count = 0;
-        for(int i = 0; i < n; i++) {
-            ll ai = nums[i] ^ k;
-            arr[i][0] = ai - nums[i];
-            arr[i][1] = ai;
-            arr[i][2] = nums[i];
-            count += arr[i][0] > 0 ? 1 : 0;
+        long long sum = 0, count = 0, sacrifice = INT_MAX;
+        for(long long num: nums){
+            sum += max((num ^ k), num);
+            count += (num ^ k) > num;
+            sacrifice = min(sacrifice, abs((num ^ k) - num));
         }
-        
-        sort(arr.begin(), arr.end(), [&](const vector<ll> a, const vector<ll> b) {
-            return a[0] < b[0];  
-        });
-        
-        if(count % 2) {
-            if(count == n) {
-                count--;
-            } else {
-                if(arr[n - count][0] + arr[n - count - 1][0] > 0)
-                    count++;
-                else
-                    count--;
-            }
-        }
-        
-        ll ans = 0;
-        for(int i = n - 1; i >= 0; i--) {
-            ans += count > 0 ? arr[i][1] : arr[i][2];
-            count--;
-        }
-        return ans;
+        return count % 2 ? sum - sacrifice : sum;
     }
 };
