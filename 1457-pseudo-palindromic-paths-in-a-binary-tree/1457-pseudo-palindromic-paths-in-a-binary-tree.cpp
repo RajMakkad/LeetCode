b16aa6f6e945c0d793da80d05d2cbb26c230aa8c
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2024-05-31 18:47:30 +0530

    Time: 223 ms (91.61%), Space: 176.8 MB (96.08%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,22 +1,14 @@
 class Solution {
-    int getPsuedoPaths(TreeNode *node, vector<int> bits){
+    int solve(TreeNode *node, int bits){
         if(!node)
             return 0;
-        bits[node->val]++;
-        if(!node->left and !node->right){
-            int odd = 0;
-            for(int i = 0;i <= 9; i++)
-                odd += (bits[i] & 1) ? 1 : 0;
-            bits[node->val]--;
-            return odd <= 1;
-        }
-        int ans = getPsuedoPaths(node->left, bits) + getPsuedoPaths(node->right, bits);
-        bits[node->val]--;
-        return ans;
+        bits = bits ^ (1 << node->val);
+        if(!node->left and !node->right)
+            return ((bits & (bits - 1)) == 0);
+        return solve(node->left, bits) + solve(node->right, bits);
     }
 public:
     int pseudoPalindromicPaths (TreeNode* root) {
-        vector<int> bits(9 + 1, 0);
-        return getPsuedoPaths(root, bits);
+        return solve(root, 0);
     }
 };
