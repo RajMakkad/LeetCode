23791bb3bd9f97d63744ad9715292a31d6ace397
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2024-06-17 01:54:47 +0530

    Time: 4 ms (66.78%), Space: 13.8 MB (45.23%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
-        long long ans = 0, mover = 0; 
-        for(int i = 0; i < nums.size(); i++){
-            if(mover >= n)
-                break;
-            while(mover + 1 < nums[i] and mover < n){
-                mover += mover + 1;
+        int ans = 0, idx = 0, m = nums.size();
+        long long access = 0;
+        while(access < n){
+            if(idx < m and access + 1 >= nums[idx]) access += nums[idx++];
+            else {
+                access = access + access + 1;
                 ans++;
             }
-            mover += nums[i];
-        }
-        while(mover < n){
-            mover += mover + 1;
-            ans++;
         }
         return ans;
     }
