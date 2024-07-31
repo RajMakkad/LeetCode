b3cf80006acf35239369598d7962d116f689b97f
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2024-08-01 00:49:46 +0530

    Time: 3 ms (87.31%), Space: 11.1 MB (49.85%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -4,15 +4,12 @@ public:
         int n = books.size();
         vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
-        dp[1] = books[0][1];
-        for(int i = 2; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
             int w = 0;
             int h = 0;
-            for(int j = i; j > 0; j--) {
+            for(int j = i; j >= 1 && w + books[j - 1][0] <= shelfWidth; j--) {
                 w += books[j - 1][0];
                 h = max(h, books[j - 1][1]);
-                if(w > shelfWidth)
-                    break;
                 dp[i] = min(dp[i], h + dp[j - 1]);
                 
             }
