cabc52776cfbcb3cca9b8b8de5461564b93ac0ed
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2024-08-05 00:00:06 +0530

    Time: 71 ms (87.64%), Space: 11.5 MB (81.35%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,19 +1,19 @@
+#define pi pair<int, int>
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int> arr;
-        for(int i = 0; i < n; i++){
-            int sum = 0;
-            for(int j = i; j < n; j++){
-                sum += nums[j];
-                arr.push_back(sum);
-            }
-        }
-        sort(arr.begin(), arr.end());
+        priority_queue<pi, vector<pi>, greater<pi>> q;
+        for(int i = 0; i < n; i++)
+            q.push({nums[i], i});
         const int mod = 1e9 + 7;
         int ans = 0;
-        for(int i = left - 1; i < right; i++){
-            ans = (ans + arr[i]) % mod;
+        for(int i = 0; i < right; i++){
+            pi t = q.top();
+            q.pop();
+            if(i >= left - 1) 
+                ans = (ans + t.first) % mod;
+            if(t.second < n - 1)
+                q.push({(t.first + nums[++t.second]) % mod, t.second});
         }
         return ans;
     }
